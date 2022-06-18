c77295ffe2238fd43b3aedded6ff72d2414bd830
cecisandoval <cecilia_sandoval@epam.com>
2022-06-18 13:32:27 -0500

    fixing provider data

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.26.0"
+      version = "~> 4.16"
     }
     random = {
       source  = "hashicorp/random"
       version = "3.0.1"
     }
   }
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.2.0"
 
   cloud {
     organization = "example-org-4f836e"
